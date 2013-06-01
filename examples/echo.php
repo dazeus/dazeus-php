2a72da04fe82a3ef37957ec13999c937a62b8d68
Ruben Nijveld <ruben@gewooniets.nl>
2013-06-01 19:17:41 +0200

    Fixed echo example

--- examples/echo.php
+++ examples/echo.php
@@ -5,6 +5,6 @@ set_time_limit(0);
 
 $dazeus = DaZeus\Factory::create('unix:///tmp/dazeus.sock');
 $dazeus->subscribe('message', function ($message) {
-    $message->reply($message->getMessage());
+    $message->reply($message->message);
 });
 $dazeus->run();
